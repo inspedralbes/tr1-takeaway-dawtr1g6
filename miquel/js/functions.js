1aa46aa93d8e9419dcd0913edd3c875aac4b60a1
mm333mm <a20miqmorcho@inspedralbes.cat>
2023-10-24 13:29:53 +0200

    cap a casa

--- miquel/js/functions.js
+++ miquel/js/functions.js
@@ -5,6 +5,9 @@ createApp({
 
     data() {
         return {
+            searchTerm: "",
+            visible: false,
+            amagar: false,
             totalCarret: 0,
             carret: [],
             productes: []
@@ -14,7 +17,7 @@ createApp({
     methods: {
 
         trobarProd(id) {
-            const carretIndex = this.carret.findIndex(item => item.nombre === this.productes[id].nombre);
+            const carretIndex = this.carret.findIndex(item => item.nom === this.productes[id].nom);
             return carretIndex;
         },
         sumar(id) {
@@ -26,6 +29,9 @@ createApp({
             }
             console.log(this.carret);
             this.calTotal();
+            if (!this.visible && !this.amagar) {
+                this.setVisible(true);
+            }
         },
         restar(id) {
             if (this.productes[id].carro > 0) {
@@ -53,12 +59,37 @@ createApp({
             }
             console.log(this.carret);
             this.calTotal();
+            if (this.carret.length < 1) {
+                this.visible = false;
+            }
         },
         calTotal() {
-
+            this.totalCarret = 0;
             this.carret.forEach(element => {
                 this.totalCarret += element.precio * element.carro;
             });
+        },
+        toggle() {
+            console.log(this.visible);
+            if (this.visible && this.carret.length!=0) {
+                this.amagar = true;
+            }
+            if (this.visible) {
+                this.visible = false;
+            } else if (!this.visible) {
+                this.visible = true;
+            }
+            console.log(this.visible);
+        },
+        setVisible(cond) {
+            this.visible = cond;
+        },
+        search() {
+            if (this.searchTerm.trim() !== "") {
+                this.productes = this.productes.filter((product) => {
+                    return product.nom.toLowerCase().includes(this.searchTerm.toLowerCase());
+                });
+            }
         }
     },
     created() {
@@ -66,4 +97,4 @@ createApp({
             this.productes = data
         });
     }
-}).mount('#cont')
+}).mount('#main')
