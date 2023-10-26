89ce064fe36fc2da0a3ac72541c961884fc1b80c
mm333mm <a20miqmorcho@inspedralbes.cat>
2023-10-26 10:17:59 +0200

    icnone de carret dinamic y barra de cerca

--- miquel/js/functions.js
+++ miquel/js/functions.js
@@ -5,6 +5,7 @@ createApp({
 
     data() {
         return {
+            carrEstat: false,
             searchTerm: "",
             visible: false,
             amagar: false,
@@ -31,6 +32,8 @@ createApp({
             this.calTotal();
             if (!this.visible && !this.amagar) {
                 this.setVisible(true);
+            } else if (!this.visible && this.amagar) {
+                this.canviCarrEstat(1);
             }
         },
         restar(id) {
@@ -72,13 +75,16 @@ createApp({
         },
         toggle() {
             console.log(this.visible);
-            if (this.visible && this.carret.length!=0) {
+            if (this.visible && this.carret.length != 0) {
                 this.amagar = true;
             }
             if (this.visible) {
                 this.visible = false;
+                document.getElementById("cart").style.display = "none";
             } else if (!this.visible) {
                 this.visible = true;
+                document.getElementById("cart").style.removeProperty("display");
+                this.carrEstat = false;
             }
             console.log(this.visible);
         },
@@ -91,11 +97,43 @@ createApp({
                     return product.name.toLowerCase().includes(this.searchTerm.toLowerCase());
                 });
             }
-        }
+        },
+        canviCarrEstat(con) {
+            if (con == 1) {
+                this.carrEstat = true;
+            } else {
+                this.carrEstat = false;
+            }
+        },
+        onSearchInput() {
+            // Cancela el temporizador anterior, si existe
+            if (this.searchTimer) {
+                clearTimeout(this.searchTimer);
+            }
+
+            // Configura un nuevo temporizador para realizar la búsqueda después de 300 ms (ajusta el valor según tus necesidades)
+            this.searchTimer = setTimeout(this.search, 3000);
+        },
+        search() {
+            if (this.searchTerm.trim() !== "") {
+                this.productos = this.productos.filter((product) => {
+                    return product.name.toLowerCase().includes(this.searchTerm.toLowerCase());
+                });
+            } else {
+                // Cuando el campo de búsqueda está vacío, muestra todos los productos nuevamente
+                getDades().then(data => {
+                    this.productos = data;
+                });
+            }
+        },
+    },
+    watch: {
+        searchTerm: "search"
     },
     created() {
         getDades().then(data => {
-            this.productos = data
+            this.productos = data;
+            this.search();
         });
     }
 }).mount('#main')
