d9ab6af60efebe28283f5164fd74f871e01c0da9
mm333mm <a20miqmorcho@inspedralbes.cat>
2023-10-27 12:47:38 +0200

    milora mobile

--- miquel/js/modules.js
+++ miquel/js/modules.js
@@ -1,5 +1,5 @@
 export async function getDades () {
-    const response = await fetch('http://autoplanet.daw.inspedralbes.cat/1-dawtr1g6/public/api/getJsonProductos');
+    const response = await fetch('./js/json/dades.json');
     // ./js/json/dades.json
     // http://autoplanet.daw.inspedralbes.cat/1-dawtr1g6/public/api/getJsonProductos
     const dades = await response.json();
