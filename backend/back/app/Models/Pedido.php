681d8e519e9c63bc98c5d94ae2585c4dd2c02a27
a22joslarfer <a22joslarfer@inspedralbes.cat>
2023-11-08 08:13:33 +0100

    update backend

---
+++ backend/back/app/Models/Pedido.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pedido extends Model
+{
+    use HasFactory;
+
+
+    protected $table = 'pedidos';
+    protected $fillable = [
+        "status",
+        "sumatori",
+        "codi_postal",
+        "direccio",
+        "ciutat",
+        "pais",
+        "namecli",
+        "user_id",
+        
+    ];
+}
