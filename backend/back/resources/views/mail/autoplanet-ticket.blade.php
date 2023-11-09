241bff056a699bdda04408eec935e54b87af5ce1
a22joslarfer <126773105+a22joslarfer@users.noreply.github.com>
2023-11-09 12:46:53 +0100

    Delete backend/back directory

--- backend/back/resources/views/mail/autoplanet-ticket.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Ticket Autoplanet</title>
-</head>
-<body>
-    <img src="data:image/png;base64,{{ base64_encode($qr) }}" alt="QR Code"> <!--- el base64_encode(var qr (compact de userInfo))-->
-</body>
-</html>
